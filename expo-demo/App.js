451b40bc30d82e3760ed07259b328fee8d697eba
Narbe Hemat Siraki <imnapo@gmail.com>
2018-12-21 00:01:08 +0330

    image selector added to demo app

--- expo-demo/App.js
+++ expo-demo/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Keyboard
-, TouchableWithoutFeedback
+, TouchableWithoutFeedback, Text
 , KeyboardAvoidingView, Platform } from 'react-native';
+import { Permissions, ImagePicker } from 'expo';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import  CNRichTextEditor , { CNToolbar, getInitialObject  } from "react-native-cn-richtext-editor";
+
+import {
+    Menu,
+    MenuOptions,
+    MenuOption,
+    MenuTrigger,
+    MenuContext,
+    MenuProvider,
+    renderers
+  } from 'react-native-popup-menu';
+const { SlideInMenu } = renderers;
 
 const IS_IOS = Platform.OS === 'ios';
 
@@ -37,84 +49,162 @@ class App extends Component {
     }
 
     
-  onSelectedTagChanged = (tag) => {
+    onSelectedTagChanged = (tag) => {
 
-    this.setState({
-        selectedTag: tag
-    })
-  }
+        this.setState({
+            selectedTag: tag
+        })
+    }
 
-  onSelectedStyleChanged = (styles) => {
-   
-    this.setState({
-        selectedStyles: styles
-    })
-}
-
-onValueChanged = (value) => {
-    this.setState({
-        value: value
-    });
-}
-
-insertImage(url) {
+    onSelectedStyleChanged = (styles) => {
     
-    this.editor.insertImage(url);
-}
+        this.setState({
+            selectedStyles: styles
+        })
+    }
+
+    onValueChanged = (value) => {
+        this.setState({
+            value: value
+        });
+    }
+
+    insertImage(url) {
+        
+        this.editor.insertImage(url);
+    }
+
+    askPermissionsAsync = async () => {
+        const camera = await Permissions.askAsync(Permissions.CAMERA);
+        const cameraRoll = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        this.setState({
+        hasCameraPermission: camera.status === 'granted',
+        hasCameraRollPermission: cameraRoll.status === 'granted'
+        });
+    };
+
+    useLibraryHandler = async () => {
+        await this.askPermissionsAsync();
+        let result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 4],
+        base64: false,
+        });
+        
+        this.insertImage(result.uri);
+    };
+
+    useCameraHandler = async () => {
+        await this.askPermissionsAsync();
+        let result = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [4, 4],
+        base64: false,
+        });
+        console.log(result);
+        
+        this.insertImage(result.uri);
+    };
+
+    onImageSelectorClicked = (value) => {
+        if(value == 1) {
+            this.useCameraHandler();    
+        }
+        else if(value == 2) {
+            this.useLibraryHandler();         
+        }
+    }
+
+    renderImageSelector() {
+        return (
+            <Menu renderer={SlideInMenu} onSelect={this.onImageSelectorClicked}>
+            <MenuTrigger>
+                <MaterialCommunityIcons name="image" size={28} />
+            </MenuTrigger>
+            <MenuOptions>
+                <MenuOption value={1}>
+                    <Text style={styles.menuOptionText}>
+                        Take Photo
+                    </Text>
+                </MenuOption>
+                <View style={styles.divider}/>
+                <MenuOption value={2} >
+                    <Text style={styles.menuOptionText}>
+                        Photo Library
+                    </Text>
+                </MenuOption> 
+                <View style={styles.divider}/>
+                <MenuOption value={3}>
+                    <Text style={styles.menuOptionText}>
+                        Cancel
+                    </Text>
+                </MenuOption>
+            </MenuOptions>
+            </Menu>
+        );
+    
+    }
 
   
-render() {
-    return (
-        <KeyboardAvoidingView
-        behavior="padding" 
-        enabled
-        keyboardVerticalOffset={IS_IOS ? 0 : 0}
-        style={{
-            flex: 1,
-            paddingTop: IS_IOS ? 20 : 0,
-            backgroundColor:'#eee',
-            flexDirection: 'column', 
-            justifyContent: 'flex-end',
-        }}
-    >
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss} >             
-                <View style={styles.main}>
-                    <CNRichTextEditor                   
-                        ref={input => this.editor = input}
-                        onSelectedTagChanged={this.onSelectedTagChanged}
-                        onSelectedStyleChanged={this.onSelectedStyleChanged}
-                        value={this.state.value}
-                        style={{ backgroundColor : '#fff', padding : 10}}
-                        //foreColor=''
-                        onValueChanged={this.onValueChanged}
-                        //onRemoveImage={this.onRemoveImage}
-                    />                        
-                </View>
-        </TouchableWithoutFeedback>
+    render() {
+        return (
+            <KeyboardAvoidingView 
+            behavior="padding" 
+            enabled
+            keyboardVerticalOffset={IS_IOS ? 0 : 0}
+            style={{
+                flex: 1,
+                paddingTop: IS_IOS ? 20 : 0,
+                backgroundColor:'#eee',
+                flexDirection: 'column', 
+                justifyContent: 'flex-end', 
+            }}
+        >
+                <MenuProvider style={{flex: 1}}>
 
-        <View style={{
-            minHeight: 35
-        }}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} >             
+                    <View style={styles.main}>
+                        <CNRichTextEditor                   
+                            ref={input => this.editor = input}
+                            onSelectedTagChanged={this.onSelectedTagChanged}
+                            onSelectedStyleChanged={this.onSelectedStyleChanged}
+                            value={this.state.value}
+                            style={{ backgroundColor : '#fff', padding : 10}}
+                            //foreColor=''
+                            onValueChanged={this.onValueChanged}
+                            //onRemoveImage={this.onRemoveImage}
+                        />                        
+                    </View>
+            </TouchableWithoutFeedback>
 
-            <CNToolbar
-                size={28}
-                bold={<MaterialCommunityIcons name="format-bold" />}
-                italic={<MaterialCommunityIcons name="format-italic" />}
-                underline={<MaterialCommunityIcons name="format-underline" />}
-                lineThrough={<MaterialCommunityIcons name="format-strikethrough-variant" />}
-                body={<MaterialCommunityIcons name="format-text" />}
-                title={<MaterialCommunityIcons name="format-header-1" />}
-                heading={<MaterialCommunityIcons name="format-header-3" />}
-                ul={<MaterialCommunityIcons name="format-list-bulleted" />}
-                ol={<MaterialCommunityIcons name="format-list-numbers" />}
-                image={<MaterialCommunityIcons name="image" />}
-                selectedTag={this.state.selectedTag}
-                selectedStyles={this.state.selectedStyles}
-                onStyleKeyPress={this.onStyleKeyPress} />
-        </View>
-    </KeyboardAvoidingView>
-    );
-}
+            <View style={{
+                minHeight: 35
+            }}>
+
+                <CNToolbar
+                    size={28}
+                    bold={<MaterialCommunityIcons name="format-bold" />}
+                    italic={<MaterialCommunityIcons name="format-italic" />}
+                    underline={<MaterialCommunityIcons name="format-underline" />}
+                    lineThrough={<MaterialCommunityIcons name="format-strikethrough-variant" />}
+                    body={<MaterialCommunityIcons name="format-text" />}
+                    title={<MaterialCommunityIcons name="format-header-1" />}
+                    heading={<MaterialCommunityIcons name="format-header-3" />}
+                    ul={<MaterialCommunityIcons name="format-list-bulleted" />}
+                    ol={<MaterialCommunityIcons name="format-list-numbers" />}
+                    // image={<MaterialCommunityIcons name="image" />}
+                    image={this.renderImageSelector()}
+                    selectedTag={this.state.selectedTag}
+                    selectedStyles={this.state.selectedStyles}
+                    onStyleKeyPress={this.onStyleKeyPress} />
+            </View>
+            </MenuProvider>
+
+        </KeyboardAvoidingView>
+        );
+    }
+
 }
 
 var styles = StyleSheet.create({
@@ -126,6 +216,17 @@ var styles = StyleSheet.create({
         paddingBottom: 1,
         alignItems: 'stretch',
     },
+    menuOptionText: {
+        textAlign: 'center',
+        paddingTop: 5,
+        paddingBottom: 5
+    },
+    divider: {
+        marginVertical: 0,
+        marginHorizontal: 0,
+        borderBottomWidth: 1,
+        borderColor: '#eee'
+    }
 });
 
 export default App;
